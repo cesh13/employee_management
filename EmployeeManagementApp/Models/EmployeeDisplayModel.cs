8ef88b8f70ffec0da301dc343d48c4b7114b4046
Cesar Molina <cesh_m@hotmail.com>
2022-10-18 18:10:09 -0600

    Implementing sortings and adding details

--- EmployeeManagementApp/Models/EmployeeDisplayModel.cs
+++ EmployeeManagementApp/Models/EmployeeDisplayModel.cs
@@ -7,17 +7,20 @@ namespace EmployeeManagementApp.Models
         
         public int EmployeeID { get; set; }
         [MaxLength(100)]
+        [MinLength(3)]
         [Required]
         public string FirstName { get; set; }
         [MaxLength(100)]
+        [MinLength(3)]
         [Required]
         public string LastName { get; set; }
         [Required]
         [MaxLength(14)]
-        [RegularExpression(@"\(?\d{3}\)? ? *\d{3}-? *-?\d{4}", ErrorMessage = "Format should match: (XXX) XXX-XXXX")]
+        [RegularExpression(@"\(\d{3}\)\s\d{3}-\d{4}", ErrorMessage = "Format should match: (XXX) XXX-XXXX")]
         public string Phone { get; set; }
         [Required]
         [MaxLength(10)]
+        [MinLength(5)]
         public string Zip { get; set; }
         [Required]
         public DateTime HireDate { get; set; }
